c306b1f9e67886de99d5a8de169ce7a729e5c187
Ricardo Murillo B <rcjmurillo@gmail.com>
2018-07-17 01:35:38 -0600

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='autoextractor',
+    version='0.1.0',
+    packages=['autoextractor'],
+    long_description='Extract and transform data for model training',
+)
